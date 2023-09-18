a280557f42c9000c2d3435064d772aa6ce818b98
BalderHolst <balderwh@gmail.com>
2023-09-18 13:17:00 +0200

    Remove non-available printers

--- static/index.js
+++ static/index.js
@@ -51,6 +51,7 @@ function createPrinterElement(printer) {
     return printer_div;
 }
 
+// Create cards for all available printers
 function populate_printers(printers) {
     const printers_div = document.getElementById("printers");
 
@@ -64,14 +65,39 @@ function populate_printers(printers) {
     }
 }
 
-// TODO: Add remove old
+// Update the shown printers
 async function update_printers() {
     const printers = await get_printers();
 
+    // Remove old printers
+    for (p_div of document.getElementsByClassName("printer")) {
+        const id = p_div.getElementsByTagName("input")[0].id;
+
+        // Is the current printer div refering to an available printer?
+        var available = false;
+
+        // Try to find the referenced printer
+        for (printer of printers) {
+            if ("printer" + printer.id === id) {
+                available = true;
+                break;
+            }
+        }
+
+        // Remove the printer from the list if it was not found
+        if (!available) {
+            p_div.remove();
+        }
+    }
+
+    // Update printers with new information, and create ones that do not exist.
     for (printer of printers) {
         let label = document.getElementById("printer" + printer.id);
 
+        // Div for holding the printer info
         var div;
+
+        // If the printer does not have a label, create one.
         if (label === null) {
             var div = createPrinterElement(printer);
             const printers_div = document.getElementById("printers");
@@ -81,10 +107,12 @@ async function update_printers() {
             var div = label.parentElement;
         }
 
+        // Remove status classes
         div.classList.remove("idle");
         div.classList.remove("running");
         div.classList.remove("error");
 
+        // Add the appropriate status class
         if (printer.status == "idle") {
             div.classList.add("idle");
         }
@@ -97,6 +125,7 @@ async function update_printers() {
     }
 }
 
+// Load the available printers on first page load
 get_printers().then(printers => populate_printers(printers));
 
 // Update printer status every 4 seconds
